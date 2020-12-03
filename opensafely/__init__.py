e1e76e79b1251be170aec326479f7984777f5a7d
David Evans <dave.evans@thedatalab.org>
2020-12-03 21:53:33 +0000

    feat: Add `update_codelists` command

--- opensafely/__init__.py
+++ opensafely/__init__.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 
 from opensafely._vendor.jobrunner import local_run
+from opensafely import update_codelists
 
 
 def main():
@@ -20,9 +21,17 @@ def main():
     parser_help = subparsers.add_parser("help", help="Show this help message and exit")
     parser_help.set_defaults(function=show_help)
 
+    # Add `run` command
     parser_run = subparsers.add_parser("run", help=local_run.DESCRIPTION)
     local_run.add_arguments(parser_run)
     parser_run.set_defaults(function=local_run.main)
+
+    # Add `update_codelists` command
+    parser_update_codelists = subparsers.add_parser(
+        "update_codelists", help=update_codelists.DESCRIPTION
+    )
+    update_codelists.add_arguments(parser_update_codelists)
+    parser_update_codelists.set_defaults(function=update_codelists.main)
 
     args = parser.parse_args()
     kwargs = vars(args)
