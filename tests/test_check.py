744a76cbcd0144c97dcbe136c478e0527a441dc0
Jon Massey <jon.massey@thedatalab.org>
2021-11-23 13:17:40 +0000

    refactor: combine study def writing methods

--- tests/test_check.py
+++ tests/test_check.py
@@ -6,27 +6,12 @@ import os
 import subprocess
 import textwrap
 from enum import Enum
+import itertools
 
 # Because we're using a vendored version of requests we need to monkeypatch the
 # requests_mock library so it references our vendored library instead
 mocker.requests = requests
 mocker._original_send = requests.Session.send
-
-
-RESTRICTED_FUNCTION_CALL = (
-    "patients.admitted_to_icu("
-    "between=['2021-01-01','2022-02-02'], "
-    "find_first_match_in_period=True, "
-    "returning='binary_flag')"
-)
-
-UNRESTRICTED_FUNCTION_CALL = (
-    "patients.with_these_medications("
-    "between=['2021-01-01','2022-02-02'], "
-    "find_first_match_in_period=True, "
-    "returning='binary_flag')"
-)
-
 
 PERMISSIONS_TEXT = """
 opensafely/dummy_icnarc:
@@ -55,32 +40,35 @@ STUDY_DEF_HEADER = """from cohortextractor import (StudyDefinition, patients)
 study = StudyDefinition ("""
 
 
-def write_restricted_files(path):
+RESTRICTED_FUNCTION_CALL = (
+    "patients.admitted_to_icu("
+    "between=['2021-01-01','2022-02-02'], "
+    "find_first_match_in_period=True, "
+    "returning='binary_flag')"
+)
+
+UNRESTRICTED_FUNCTION_CALL = (
+    "patients.with_these_medications("
+    "between=['2021-01-01','2022-02-02'], "
+    "find_first_match_in_period=True, "
+    "returning='binary_flag')"
+)
+
+
+def write_study_def(path, restricted):
     for a in [1, 2]:
-        f = path / f"study_definition_restricted_{a}.py"
+        f = (
+            path
+            / f"study_definition_{'' if restricted else 'un'}restricted_{a}.py"
+        )
         f.write_text(
             textwrap.dedent(
                 f"""\
                 {STUDY_DEF_HEADER}
-                a={RESTRICTED_FUNCTION_CALL},
+                {'' if restricted else '#'}a={RESTRICTED_FUNCTION_CALL},
                 #b={RESTRICTED_FUNCTION_CALL},
                 c={UNRESTRICTED_FUNCTION_CALL},
                 #d={UNRESTRICTED_FUNCTION_CALL},
-                )"""
-            )
-        )
-
-
-def write_unrestricted_files(path):
-    for a in [1, 2]:
-        f = path / f"study_definition_unrestricted_{a}.py"
-        f.write_text(
-            textwrap.dedent(
-                f"""\
-                {STUDY_DEF_HEADER}
-                #a={RESTRICTED_FUNCTION_CALL},
-                b={UNRESTRICTED_FUNCTION_CALL},
-                #c={UNRESTRICTED_FUNCTION_CALL},
                 )"""
             )
         )
@@ -124,14 +112,18 @@ def repo_path(tmp_path):
     os.chdir(prev_dir)
 
 
-def run_test(repo_path, capsys, monkeypatch, repo, protocol, restricted):
+@pytest.mark.parametrize(
+    "repo, protocol, restricted",
+    itertools.chain(
+        itertools.product(list(Repo), list(Protocol), [True, False]),
+        itertools.product([None], [None], [True, False]),
+    ),
+)
+def test_check(repo_path, capsys, monkeypatch, repo, protocol, restricted):
     if "GITHUB_REPOSITORY" in os.environ:
         monkeypatch.delenv("GITHUB_REPOSITORY")
 
-    if restricted:
-        write_restricted_files(repo_path)
-    else:
-        write_unrestricted_files(repo_path)
+    write_study_def(repo_path, restricted)
 
     if repo:
         if protocol == Protocol.ENVIRON:
@@ -149,287 +141,3 @@ def run_test(repo_path, capsys, monkeypatch, repo, protocol, restricted):
         validate_fail(capsys)
     else:
         validate_pass(capsys)
-
-
-def test_unrestricted_local_norepo(repo_path, capsys, monkeypatch):
-    run_test(repo_path, capsys, monkeypatch, repo=None, protocol=None, restricted=False)
-
-
-def test_restricted_local_norepo(repo_path, capsys, monkeypatch):
-    run_test(repo_path, capsys, monkeypatch, repo=None, protocol=None, restricted=True)
-
-
-def test_unrestricted_unknown_local_https(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNKNOWN,
-        Protocol.HTTPS,
-        restricted=False,
-    )
-
-
-def test_unrestricted_unknown_local_ssh(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNKNOWN,
-        Protocol.SSH,
-        restricted=False,
-    )
-
-
-def test_unrestricted_permitted_multiple_local_https(
-    repo_path, capsys, monkeypatch
-):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED_MULTIPLE,
-        Protocol.HTTPS,
-        restricted=False,
-    )
-
-
-def test_unrestricted_permitted_multiple_local_ssh(
-    repo_path, capsys, monkeypatch
-):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED_MULTIPLE,
-        Protocol.SSH,
-        restricted=False,
-    )
-
-
-def test_restricted_unknown_local_https(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNKNOWN,
-        Protocol.HTTPS,
-        restricted=True,
-    )
-
-
-def test_restricted_unknown_local_ssh(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNKNOWN,
-        Protocol.SSH,
-        restricted=True,
-    )
-
-
-def test_restricted_permitted_multiple_local_https(
-    repo_path, capsys, monkeypatch
-):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED_MULTIPLE,
-        Protocol.HTTPS,
-        restricted=True,
-    )
-
-
-def test_restricted_permitted_multiple_local_ssh(
-    repo_path, capsys, monkeypatch
-):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED_MULTIPLE,
-        Protocol.SSH,
-        restricted=True,
-    )
-
-
-def test_restricted_permitted_local_https(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED_MULTIPLE,
-        Protocol.HTTPS,
-        restricted=True,
-    )
-
-
-def test_restricted_permitted_local_ssh(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED,
-        Protocol.SSH,
-        restricted=True,
-    )
-
-
-def test_unrestricted_permitted_local_https(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED,
-        Protocol.HTTPS,
-        restricted=False,
-    )
-
-
-def test_unrestricted_permitted_local_ssh(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED,
-        Protocol.SSH,
-        restricted=False,
-    )
-
-
-def test_restricted_unpermitted_local_https(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNPERMITTED,
-        Protocol.HTTPS,
-        restricted=True,
-    )
-
-
-def test_restricted_unpermitted_local_ssh(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNPERMITTED,
-        Protocol.SSH,
-        restricted=True,
-    )
-
-
-def test_unrestricted_unpermitted_local_https(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNPERMITTED,
-        Protocol.HTTPS,
-        restricted=False,
-    )
-
-
-def test_unrestricted_unpermitted_local_ssh(repo_path, capsys, monkeypatch):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNPERMITTED,
-        Protocol.SSH,
-        restricted=False,
-    )
-
-
-def test_unrestricted_unpermitted_action(monkeypatch, repo_path, capsys):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNPERMITTED,
-        Protocol.ENVIRON,
-        restricted=False,
-    )
-
-
-def test_unrestricted_permitted_action(monkeypatch, repo_path, capsys):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED,
-        Protocol.ENVIRON,
-        restricted=False,
-    )
-
-
-def test_restricted_permitted_action(monkeypatch, repo_path, capsys):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED,
-        Protocol.ENVIRON,
-        restricted=True,
-    )
-
-
-def test_restricted_unpermitted_action(monkeypatch, repo_path, capsys):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNPERMITTED,
-        Protocol.ENVIRON,
-        restricted=True,
-    )
-
-
-def test_restricted_permitted_multiple_action(monkeypatch, repo_path, capsys):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED_MULTIPLE,
-        Protocol.ENVIRON,
-        restricted=True,
-    )
-
-
-def test_unrestricted_permitted_multiple_action(
-    monkeypatch, repo_path, capsys
-):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.PERMITTED_MULTIPLE,
-        Protocol.ENVIRON,
-        restricted=False,
-    )
-
-
-def test_restricted_unknown_action(monkeypatch, repo_path, capsys):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNKNOWN,
-        Protocol.ENVIRON,
-        restricted=True,
-    )
-
-
-def test_unrestricted_nonexistent_multiple_action(
-    monkeypatch, repo_path, capsys
-):
-    run_test(
-        repo_path,
-        capsys,
-        monkeypatch,
-        Repo.UNKNOWN,
-        Protocol.ENVIRON,
-        restricted=True,
-    )
