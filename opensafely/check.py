de06f965a38e4da57af3c8e84aba70bdcabc9e2b
Jon Massey <jon.massey@thedatalab.org>
2021-11-25 10:49:54 +0000

    Check datasources used (#75)
    * feat: check datasources used in study definition
    
      - ascertain git repository name from local git config or env var
      - download repository_permissions.yaml and lookup repo name
      - check any python file within repo for usage of functions which access restricted data
        - except if repo has permissions defined in repository_permissions.yaml
    
    * feat: unit tests covering basic scenarios
    
    Covers:
    * no repository initialised
      * no restricted functions (RFs) - Pass check
      * RFs used - Fail check
    * repository not in permissions list
      * no RFs - Pass check
      * RFs - Fail check
    * repository in permissions list but not permitted to use dataset
      * no RFs - Pass check
      * RFs - Fail check
    * repository in permissions list and permitted to use dataset
      * no RFs - Pass check
      * RFs - Pass check
    * repository in permissions list and permitted to use dataset and others
      * no RFs - Pass check
      * RFs - Pass check
    
    HTTPS and SSH URLs to GitHub remotes for ascertainment of
    repository name covered for "local run" scenario,
    as well as using GitHub environment variables for when
    run as GitHub action.
    
    Allow override via environment variable for testing.
    
    Add test for correctness of "live" permisisons yaml.

---
+++ opensafely/check.py
@@ -0,0 +1,116 @@
+from opensafely._vendor import requests
+from opensafely._vendor.ruamel.yaml import YAML
+import os
+import re
+import glob
+import subprocess
+import sys
+
+DESCRIPTION = "Check the opensafely project for correctness"
+
+RESTRICTED_DATASETS = {"icnarc": ["admitted_to_icu"]}
+
+PERMISSIONS_URL = "https://raw.githubusercontent.com/opensafely-core/opensafely-cli/main/repository_permisisons.yaml"
+
+def add_arguments(parser):
+    pass
+
+
+def main():
+    permissions_url = (
+        os.environ.get("OPENSAFELY_PERMISSIONS_URL")
+        or PERMISSIONS_URL
+    )
+    repo_name = get_repository_name()
+    permissions = get_datasource_permissions(permissions_url)
+    allowed_datasets = get_allowed_datasets(repo_name, permissions)
+    datasets_to_check = {
+        k: v
+        for k, v in RESTRICTED_DATASETS.items()
+        if k not in allowed_datasets
+    }
+    files_to_check = glob.glob("**/*.py", recursive=True)
+
+    found_datasets = {
+        dataset: check_dataset(functions, files_to_check)
+        for dataset, functions in datasets_to_check.items()
+        if check_dataset(functions, files_to_check)
+    }
+
+    if found_datasets:
+        violations = "\n".join(format_violations(found_datasets))
+        sys.exit(violations)
+    else:
+        print("Success")
+
+
+def format_violations(found_datasets):
+    yield "Usage of restricted datasets found:\n"
+    for d, functions in found_datasets.items():
+        yield f"{d}:"
+        for fn, files in functions.items():
+            yield f"- {fn}"
+            for f, lines in files.items():
+                yield f"  - {f}:"
+                for ln, line in lines.items():
+                    yield f"    line {ln}: {line}"
+
+
+def check_dataset(functions, files_to_check):
+    found_functions = {}
+    for function in functions:
+        regex = re.compile(rf"\.{function}\(")
+        found_files = {}
+        for f in files_to_check:
+            matches = check_file(f, regex)
+            if matches:
+                found_files[f] = matches
+        if found_files:
+            found_functions[function] = found_files
+    return found_functions
+
+
+def check_file(filename, regex):
+    found_lines = {}
+    with open(filename, "r") as f:
+        for ln, line in enumerate(f.readlines(), start=1):
+            if line.lstrip().startswith("#"):
+                continue
+            if regex.search(line):
+                found_lines[ln] = line
+    return found_lines
+
+
+def get_datasource_permissions(permissions_url):
+    resp = requests.get(permissions_url)
+    if resp.status_code != 200:
+        raise requests.RequestException(
+            f"Error {resp.status_code} getting {permissions_url}"
+        )
+    yaml = YAML()
+    permissions = yaml.load(resp.text)
+    return permissions
+
+
+def get_repository_name():
+    if "GITHUB_REPOSITORY" in os.environ:
+        return os.environ["GITHUB_REPOSITORY"]
+    else:
+
+        url = subprocess.run(
+            args=["git", "config", "--get", "remote.origin.url"],
+            capture_output=True,
+            text=True,
+        ).stdout
+        return (
+            url.replace("https://github.com/", "")
+            .replace("git@github.com:", "")
+            .replace(".git", "")
+            .strip()
+        )
+
+
+def get_allowed_datasets(respository_name, permissions):
+    if not respository_name or respository_name not in permissions:
+        return []
+    return permissions[respository_name]["allow"]
---
+++ tests/test_check.py
@@ -0,0 +1,163 @@
+import pytest
+from requests_mock import mocker
+from opensafely._vendor import requests
+from opensafely import check
+import os
+import subprocess
+import textwrap
+from enum import Enum
+import itertools
+from opensafely._vendor.ruamel.yaml.comments import CommentedMap
+
+# Because we're using a vendored version of requests we need to monkeypatch the
+# requests_mock library so it references our vendored library instead
+mocker.requests = requests
+mocker._original_send = requests.Session.send
+
+PERMISSIONS_TEXT = """
+opensafely/dummy_icnarc:
+    allow: ['icnarc']
+opensafely/dummy_ons:
+    allow: ['ons']
+opensafely/dummy_icnarc_ons:
+    allow: ['icnarc','ons']
+"""
+
+PERMISSIONS_URL = "https://raw.githubusercontent.com/opensafely-core/opensafely-cli/main/tests/fixtures/permissions/repository_permisisons.yaml"
+
+class Repo(Enum):
+    PERMITTED = "opensafely/dummy_icnarc"
+    PERMITTED_MULTIPLE = "opensafely/dummy_icnarc_ons"
+    UNPERMITTED = "opensafely/dummy_ons"
+    UNKNOWN = "opensafely/dummy"
+
+
+class Protocol(Enum):
+    HTTPS = 1
+    SSH = 2
+    ENVIRON = 3
+
+
+class Dataset(Enum):
+    RESTRICTED = True
+    UNRESTRICTED = False
+
+
+STUDY_DEF_HEADER = """from cohortextractor import (StudyDefinition, patients)
+study = StudyDefinition ("""
+
+
+UNRESTRICTED_FUNCTION = "with_these_medications"
+RESTRICTED_FUNCTION = "admitted_to_icu"
+
+
+def format_function_call(func):
+    return (
+        f"patients.{func}("
+        "between=['2021-01-01','2022-02-02'], "
+        "find_first_match_in_period=True, "
+        "returning='binary_flag')"
+    )
+
+
+def write_study_def(path, dataset):
+    restricted = dataset.value
+    for a in [1, 2]:
+        f = (
+            path
+            / f"study_definition_{'' if restricted else 'un'}restricted_{a}.py"
+        )
+        f.write_text(
+            textwrap.dedent(
+                f"""\
+                {STUDY_DEF_HEADER}
+                {'a='+format_function_call(RESTRICTED_FUNCTION)+',' if restricted else ''}
+                #b={format_function_call(RESTRICTED_FUNCTION)},
+                c={format_function_call(UNRESTRICTED_FUNCTION)},
+                #d={format_function_call(UNRESTRICTED_FUNCTION)},
+                )"""
+            )
+        )
+
+
+def git_init(url):
+    subprocess.run(["git", "init"])
+    subprocess.run(["git", "remote", "add", "origin", url])
+
+
+def validate_pass(capsys):
+    check.main()
+    stdout, stderr = capsys.readouterr()
+    assert stderr == ""
+    assert stdout == "Success\n"
+
+
+def validate_fail(capsys):
+    with pytest.raises(SystemExit):
+        check.main()
+        stdout, stderr = capsys.readouterr()
+        assert stdout != "Success\n"
+        assert stderr.startswith("Usage of restricted datasets found:")
+        assert "icnarc" in stderr
+        assert "admitted_to_icu" in stderr
+        assert "3:" in stderr
+        assert "4:" not in stderr
+        assert "5:" not in stderr
+        assert "#b=" not in stderr
+        assert "#d=" not in stderr
+        assert "unrestricted" not in stderr
+        assert "study_definition_restricted_1.py" in stderr
+        assert "study_definition_restricted_2.py" in stderr
+
+
+@pytest.fixture
+def repo_path(tmp_path):
+    prev_dir = os.getcwd()
+    os.chdir(tmp_path)
+    yield tmp_path
+    os.chdir(prev_dir)
+
+
+@pytest.mark.parametrize(
+    "repo, protocol, dataset",
+    itertools.chain(
+        itertools.product(list(Repo), list(Protocol), list(Dataset)),
+        itertools.product([None], [None], list(Dataset)),
+    ),
+)
+def test_check(repo_path, capsys, monkeypatch, repo, protocol, dataset):
+    if "GITHUB_REPOSITORY" in os.environ:
+        monkeypatch.delenv("GITHUB_REPOSITORY")
+
+    monkeypatch.setenv("OPENSAFELY_PERMISSIONS_URL",PERMISSIONS_URL)
+
+    write_study_def(repo_path, dataset)
+
+    if repo:
+        repo_name = repo.value
+        if protocol == Protocol.ENVIRON:
+            monkeypatch.setenv("GITHUB_REPOSITORY", repo_name)
+        else:
+            if protocol == Protocol.SSH:
+                url = f"git@github.com:{repo_name}.git"
+            elif protocol == Protocol.HTTPS:
+                url = f"https://github.com/{repo_name}"
+            else:
+                url = ""
+            git_init(url)
+
+    if dataset == Dataset.RESTRICTED and repo not in [
+        Repo.PERMITTED,
+        Repo.PERMITTED_MULTIPLE,
+    ]:
+        validate_fail(capsys)
+    else:
+        validate_pass(capsys)
+
+def test_repository_permissions_yaml():
+    permissions = check.get_datasource_permissions(check.PERMISSIONS_URL)
+    assert permissions, "empty permissions file"
+    assert type(permissions) == CommentedMap , "invalid permissions file"
+    for k,v in permissions.items():
+        assert len(v.keys()) == 1, f"multiple keys specified for {k}"
+        assert "allow" in v.keys(), f"allow key not present for {k}"
