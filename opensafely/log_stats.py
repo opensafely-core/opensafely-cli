4d968fd49e6841f152ce3423ac6fa49999eb380b
Becky Smith <becky.smith@thedatalab.org>
2022-04-26 11:11:05 +0100

    Format logs as JSONL for honeycomb

--- opensafely/log_stats.py
+++ opensafely/log_stats.py
@@ -5,8 +5,9 @@ from argparse import ArgumentParser
 from datetime import datetime
 from pathlib import Path
 
-DESCRIPTION = "Parse cohortextractor-stats logs as json"
-PREFIX_REGEX = r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{9}Z"
+DESCRIPTION = "Parse and output cohortextractor-stats logs as JSONL"
+TIMESTAMP_PREFIX_REGEX = r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{9}Z"
+COMPLETED_PREFIX_REGEX = r"^Completed successfully"
 KEY_VALUE_REGEX = r"(?<=\s)([^\s=]+)=(.*?)(?=(?:\s[^\s=]+=|$))"
 ACTION_SUMMARY_REGEX = (
     r"^(state|docker_image_id|job_id|run_by_user|created_at|completed_at):\s(.*)"
@@ -20,49 +21,90 @@ def add_arguments(parser):
         type=Path,
         default=".",
     )
+    parser.add_argument(
+        "--project-name",
+        help="Project name (default: project-dir)",
+        type=str,
+    )
+    parser.add_argument(
+        "--output-file",
+        help="Output json filename (default: log_stats.json)",
+        type=Path,
+        default="log_stats.json",
+    )
     return parser
 
 
-def add_parsed_log(stats_dict, current_action, current_log):
-    current_log_timestamp = re.match(PREFIX_REGEX, current_log).group()
-    current_log = re.sub(r"\s*\n\s*", " ", current_log)
+def _timestamp_for_honeytail(timestamp, ts_format):
+    honeytail_ts_format = "%Y-%m-%dT%H:%M:%S.000000000Z"
+    dt = datetime.strptime(timestamp, ts_format)
+    return dt.strftime(honeytail_ts_format)
+
+
+def parse_log(project_name, current_action, job_id, current_log):
+    current_log_timestamp = re.match(TIMESTAMP_PREFIX_REGEX, current_log).group()
+    current_log = re.sub(r"\s*\n\s*", " ", current_log).strip()
     log_params = dict(re.findall(KEY_VALUE_REGEX, current_log))
-    stats_dict[current_action].append(
-        {"timestamp": current_log_timestamp, **log_params}
-    )
+    ts_format = "%Y-%m-%dT%H:%M:%S"
+    return {
+        "timestamp": current_log_timestamp,
+        "project": project_name,
+        "job_id": job_id,
+        "action": current_action,
+        **log_params,
+    }
 
 
-def add_action_stats(stats_dict, current_action, action_stats):
-    start_time = action_stats.get("created_at")
-    end_time = action_stats.get("completed_at")
+def parse_stats_logs(raw_logs, project_name, current_action, job_id):
+    for log in raw_logs:
+        yield parse_log(project_name, current_action, job_id, log)
+
+
+def format_summary_stats(project_name, current_action, summary_stats):
+    start_time = summary_stats.get("created_at")
+    end_time = summary_stats.get("completed_at")
     ts_format = "%Y-%m-%dT%H:%M:%SZ"
+    start_dt = datetime.strptime(start_time, ts_format)
+
     if start_time and end_time:
-        elapsed = datetime.strptime(end_time, ts_format) - datetime.strptime(
-            start_time, ts_format
-        )
-        action_stats["elapsed_time_secs"] = elapsed.seconds
-        action_stats["elapsed_time"] = str(elapsed)
-    stats_dict[current_action].insert(0, action_stats)
+        elapsed = datetime.strptime(end_time, ts_format) - start_dt
+        summary_stats["elapsed_time_secs"] = elapsed.seconds
+        summary_stats["elapsed_time"] = str(elapsed)
+    return {
+        "timestamp": _timestamp_for_honeytail(start_time, ts_format),
+        "project": project_name,
+        "action": current_action,
+        **{k: v for k, v in summary_stats.items()},
+    }
 
 
-def main(project_dir):
-    # Log the number of actions first
-    stats = {"project": {"number_of_actions": 0}}
-
+def main(project_dir, output_file, project_name=None):
     log_dir = project_dir / "metadata"
+    project_name = project_name or project_dir.resolve().name
 
-    for filep in (log_dir).glob("*.log"):
+    log_files = list((log_dir).glob("*.log"))
+
+    # Find the number of actions (equivalent to the number of log files)
+    action_count = len(log_files)
+    stats_logs = []
+    summary_stats_logs = []
+
+    for filep in log_files:
+        action_count += 1
         current_action = filep.stem
-        stats[current_action] = []
-        action_stats = {}
+        # Include the total number of actions run alongside this one in each action summary
+        summary_stats = {"total_actions_in_job_request": action_count}
+        raw_logs = []
         current_log = None
         for line in filep.open().readlines():
             # Logs in the log file can span multiple lines;
-            # check if this line is the beginning of a new log
-            if re.match(PREFIX_REGEX, line):
+            # check if this line is the beginning of a new log or the action summary
+            if re.match(TIMESTAMP_PREFIX_REGEX, line) or re.match(
+                COMPLETED_PREFIX_REGEX, line
+            ):
                 # If there's a current log, we're finished with it now, parse it add to the stats dict
                 if current_log is not None:
-                    add_parsed_log(stats, current_action, current_log)
+                    raw_logs.append(current_log)
                 # Now reset the current log, to the current line if it's a cohort-extractor log
                 if "cohortextractor-stats" in line:
                     current_log = line
@@ -71,27 +113,43 @@ def main(project_dir):
                     current_log = None
             elif re.match(ACTION_SUMMARY_REGEX, line):
                 # Check for the summary stats lines
-                action_stats.update(dict(re.findall(ACTION_SUMMARY_REGEX, line)))
+                summary_stats.update(dict(re.findall(ACTION_SUMMARY_REGEX, line)))
             elif current_log is not None:
                 # this isn't a log start, and the previous log start was a stats one, so it
                 # must be a continuation
                 current_log += line
-        # Parse the final log, if there is one
+        # Add the final log, if there is one
         if current_log is not None:
-            add_parsed_log(stats, current_action, current_log)
-        add_action_stats(stats, current_action, action_stats)
-        stats["project"]["number_of_actions"] += 1
+            raw_logs.append(current_log)
 
-    with (log_dir / "log_stats.json").open("w") as outpath:
-        json.dump(stats, outpath, indent=2)
-        print(f"Log stats written to {log_dir / 'log_stats.json'}")
+        # format the raw logs and add to the main list
+        stats_logs.extend(
+            parse_stats_logs(
+                raw_logs, project_name, current_action, summary_stats["job_id"]
+            )
+        )
+
+        # Format and add the summary stats
+        summary_stats_logs.append(
+            format_summary_stats(project_name, current_action, summary_stats)
+        )
+
+    with (log_dir / output_file).open("w") as outpath:
+        for log in [*summary_stats_logs, *stats_logs]:
+            json.dump(log, outpath)
+            outpath.write("\n")
+    print(f"Log stats written to {log_dir / output_file}")
 
 
 def run():
     parser = ArgumentParser(description=DESCRIPTION)
     parser = add_arguments(parser)
     args = parser.parse_args()
-    main(project_dir=args.project_dir / "metadata")
+    main(
+        project_dir=args.project_dir / "metadata",
+        output_file=args.output_file,
+        project_name=args.project_name,
+    )
 
 
 if __name__ == "__main__":
--- tests/test_log_stats.py
+++ tests/test_log_stats.py
@@ -15,19 +15,27 @@ def project_path(tmp_path):
 
 
 def test_log_stats(project_path):
-    log_stats.main(project_path)
-    stats_output_path = project_path / "metadata" / "log_stats.json"
+    log_stats.main(project_path, "stats.json")
+    stats_output_path = project_path / "metadata" / "stats.json"
     assert stats_output_path.exists()
 
-    stats_json = json.load(stats_output_path.open())
-    assert sorted(stats_json.keys()) == ["action1", "action2", "project"]
+    stats_json = [json.loads(line) for line in stats_output_path.open().readlines()]
+    # sort logs by action, with summary log at th
+    stats_json.sort(key=lambda x: (x["action"], x.get("state", "")))
+    # action 1 has 8 stats logs plus one summary log
+    # action 2 has 9 stats logs plus one summary log
+    assert len(stats_json) == 9 + 10
+    summary_logs = [stats_json[8], stats_json[-1]]
+    action1_logs = stats_json[0:8]
+    action2_logs = stats_json[10:-1]
 
-    assert stats_json["project"]["number_of_actions"] == 2
-    assert len(stats_json["action1"]) == 9
-    assert len(stats_json["action2"]) == 10
-    for action in ["action1", "action2"]:
+    for summary_log in summary_logs:
         # First item in each action log list is the summary
-        assert list(stats_json[action][0].keys()) == [
+        assert list(summary_log.keys()) == [
+            "timestamp",
+            "project",
+            "action",
+            "total_actions_in_job_request",
             "state",
             "docker_image_id",
             "job_id",
@@ -39,5 +47,18 @@ def test_log_stats(project_path):
         ]
         # elapsed time is calculated from created/completed time and reported in
         # seconds and human-readable H:M:S
-        assert stats_json[action][0]["elapsed_time_secs"] == 16
-        assert stats_json[action][0]["elapsed_time"] == "0:00:16"
+        assert summary_log["elapsed_time_secs"] == 16
+        assert summary_log["elapsed_time"] == "0:00:16"
+
+    # all other logs have the job id, project etc added
+    def _assert_keys(log):
+        for key in ["job_id", "timestamp", "project", "action"]:
+            assert key in log
+
+    for statslog in action1_logs:
+        _assert_keys(statslog)
+        assert statslog["job_id"] == "3e6blwbtbxgpm6ji"
+
+    for statslog in action2_logs:
+        _assert_keys(statslog)
+        assert statslog["job_id"] == "yf5dm6ekrw7cv5vo"
