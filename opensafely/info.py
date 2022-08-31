e15f6656347498ef16317934837f85eb78a7247e
bloodearnest <simon.davy@thedatalab.org>
2022-08-31 12:11:33 +0100

    Add basic info command
    The goal is to help users be more aware of their docker resources.

---
+++ opensafely/info.py
@@ -0,0 +1,34 @@
+import sys
+import json
+import subprocess
+import opensafely
+
+DESCRIPTION = "Print information about the opensafely tool and available resources"
+
+
+def add_arguments(parser):
+    pass
+
+
+
+INFO = """
+opensafely version: {version}
+docker version: {docker_version}
+docker memory: {memory}
+docker cpu: {cpu}
+"""
+
+def main():
+    try:
+        ps = subprocess.run(["docker", "info", "-f", "{{json .}}"], capture_output=True, text=True)
+        info = json.loads(ps.stdout)
+        
+        memory = float(info["MemTotal"]) / (1024**3)
+        print(INFO.format(
+            version=opensafely.__version__,
+            docker_version=info["ServerVersion"],
+            memory=f"{memory:.1f}G",
+            cpu=info["NCPU"],
+        ))
+    except Exception:
+        sys.exit("Error retreiving docker information")
