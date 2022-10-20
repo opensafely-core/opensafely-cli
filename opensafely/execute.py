76cb7d760a9d5e25899d25e207e1875ec7acf761
David Evans <dave.evans@thedatalab.org>
2022-10-20 18:11:16 +0100

    feat: Add `opensafely exec` command
    This is for running ad-hoc OpenSAFELY actions. As an illustration,
    running this command:
    
        opensafely exec foo:v1 do_something --a=1
    
    Should do roughly the same thing as this action in `project.yaml`:
    
        my_action:
          run: foo:v1 do_something --a=1
    
    The key difference is that rather than having `needs` and `outputs`
    control which files are available, we mount the entire workspace in so
    the command can read and write any files it likes.
    
    This is motivated particularly by a desire to remove friction from the
    Data Builder tutorial. Being able to run commands, see log and error
    output immediately, and read and create arbitrary files provides a much
    smoother on-ramp than forcing everything through `project.yaml`.
    
    It also means we can support commands like Data Builder's
    `create-dummy-tables` which are development tools that don't fit
    naturally into `project.yaml` at all.
    
    Note that this command is only usable with images that have been rebuilt
    using the latest base-docker image, following this change:
    https://github.com/opensafely-core/base-docker/pull/17
    
    There are various areas for improvement and expansion here which I
    haven't attempted to tackle at this stage:
    
     * Adding an `--entrypoint` argument to allow customising the image
       entrypoint for debugging purposes.
     * Passing the appropriate product key for the `stata-mp` image.
     * Setting the `OPENSAFELY_BACKEND` environment variable to
       `expectaions` at the local job-runner does.

---
+++ opensafely/execute.py
@@ -0,0 +1,56 @@
+import argparse
+import os
+import pathlib
+import subprocess
+
+from opensafely._vendor.jobrunner import config
+from opensafely._vendor.jobrunner.cli.local_run import docker_preflight_check
+
+DESCRIPTION = "Run an OpenSAFELY action outside of the `project.yaml` pipeline"
+
+
+def add_arguments(parser):
+    parser.add_argument(
+        "image",
+        metavar="IMAGE_NAME:VERSION",
+        help="OpenSAFELY image and version (e.g. databuilder:v1)",
+    )
+    parser.add_argument(
+        "docker_args",
+        nargs=argparse.REMAINDER,
+        metavar="...",
+        help="Any additional arguments to pass to the image",
+    )
+    return parser
+
+
+def main(image, docker_args):
+    if not docker_preflight_check():
+        return False
+
+    try:
+        # In order for any files that get created to have the appropriate owner/group we
+        # run the command using the current user's UID/GID
+        uid = os.getuid()
+        gid = os.getgid()
+    except Exception:
+        # These aren't available on Windows; but then on Windows we don't have to deal
+        # with the same file ownership problems which require us to match the UID in the
+        # first place.
+        user_args = []
+    else:
+        user_args = ["--user", f"{uid}:{gid}"]
+
+    proc = subprocess.run(
+        [
+            "docker",
+            "run",
+            "--rm",
+            "-it",
+            f"--volume={pathlib.Path.cwd()}:/workspace",
+            *user_args,
+            f"{config.DOCKER_REGISTRY}/{image}",
+            *docker_args,
+        ]
+    )
+    return proc.returncode == 0
