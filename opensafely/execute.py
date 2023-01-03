9fb867d862ba9c741ade46e8ac02ffbc7438e6a0
bloodearnest <simon.davy@thedatalab.org>
2023-01-03 15:27:31 +0000

    fix: use run_docker with exec command
    Removes the execute specific code and uses utils.run_docker. This
    re-adds support for interactivity, which had to be removed.
    
    This uses the new argparse test helper for the tests.

--- opensafely/execute.py
+++ opensafely/execute.py
@@ -1,9 +1,6 @@
 import argparse
-import os
-import pathlib
-import subprocess
 
-from opensafely._vendor.jobrunner import config
+from opensafely import utils
 from opensafely._vendor.jobrunner.cli.local_run import docker_preflight_check
 
 
@@ -17,7 +14,7 @@ def add_arguments(parser):
         help="OpenSAFELY image and version (e.g. databuilder:v1)",
     )
     parser.add_argument(
-        "docker_args",
+        "cmd_args",
         nargs=argparse.REMAINDER,
         metavar="...",
         help="Any additional arguments to pass to the image",
@@ -25,32 +22,9 @@ def add_arguments(parser):
     return parser
 
 
-def main(image, docker_args):
+def main(image, cmd_args):
     if not docker_preflight_check():
         return False
 
-    try:
-        # In order for any files that get created to have the appropriate owner/group we
-        # run the command using the current user's UID/GID
-        uid = os.getuid()
-        gid = os.getgid()
-    except Exception:
-        # These aren't available on Windows; but then on Windows we don't have to deal
-        # with the same file ownership problems which require us to match the UID in the
-        # first place.
-        user_args = []
-    else:
-        user_args = ["--user", f"{uid}:{gid}"]
-
-    proc = subprocess.run(
-        [
-            "docker",
-            "run",
-            "--rm",
-            f"--volume={pathlib.Path.cwd()}:/workspace",
-            *user_args,
-            f"{config.DOCKER_REGISTRY}/{image}",
-            *docker_args,
-        ]
-    )
+    proc = utils.run_docker([], image, cmd_args, interactive=True)
     return proc.returncode
--- tests/test_execute.py
+++ tests/test_execute.py
@@ -1,46 +1,24 @@
 import pathlib
-from unittest import mock
 
 from opensafely import execute
+from tests.conftest import run_main
 
 
-@mock.patch("opensafely.execute.os")
-def test_execute_main(mock_os, run):
-    mock_os.getuid.return_value = 12345
-    mock_os.getgid.return_value = 67890
+def test_execute_main(run, no_user):
     run.expect(["docker", "info"])
     run.expect(
         [
             "docker",
             "run",
             "--rm",
-            f"--volume={pathlib.Path.cwd()}:/workspace",
-            "--user",
-            "12345:67890",
+            "--init",
+            "--label=opensafely",
+            "-it",
+            f"--volume={pathlib.Path.cwd()}://workspace",
             "ghcr.io/opensafely-core/databuilder:v1",
             "foo",
             "bar",
             "baz",
         ]
     )
-    execute.main("databuilder:v1", ["foo", "bar", "baz"])
-
-
-@mock.patch("opensafely.execute.os")
-def test_execute_main_on_windows(mock_os, run):
-    mock_os.getuid.side_effect = AttributeError()
-    mock_os.getgid.side_effect = AttributeError()
-    run.expect(["docker", "info"])
-    run.expect(
-        [
-            "docker",
-            "run",
-            "--rm",
-            f"--volume={pathlib.Path.cwd()}:/workspace",
-            "ghcr.io/opensafely-core/databuilder:v1",
-            "foo",
-            "bar",
-            "baz",
-        ]
-    )
-    execute.main("databuilder:v1", ["foo", "bar", "baz"])
+    assert run_main(execute, "databuilder:v1 foo bar baz") == 0
