841962d30ecc153f024e81913991e371e69a40c2
Tom Palmer <remlapmot@hotmail.com>
2024-09-12 09:56:46 +0100

    feat: add rstudio subcommand

---
+++ tests/test_rstudio.py
@@ -0,0 +1,38 @@
+import os
+import pathlib
+from sys import platform
+
+from opensafely import rstudio
+from tests.conftest import run_main
+
+
+def test_rstudio(run):
+    if platform == "linux":
+        uid = os.getuid()
+    else:
+        uid = None
+
+    run.expect(
+        [
+            "docker",
+            "run",
+            "--rm",
+            "--init",
+            "--label=opensafely",
+            "--interactive",
+            "--user=0:0",
+            f"--volume={pathlib.Path.cwd()}://workspace",
+            "--platform=linux/amd64",
+            "-p=8787:8787",
+            "--name=test_rstudio",
+            "--hostname=test_rstudio",
+            "--volume="
+            + os.path.join(os.path.expanduser("~"), ".gitconfig")
+            + ":/home/rstudio/local-gitconfig",
+            "--env=HOSTPLATFORM=" + platform,
+            f"--env=HOSTUID={uid}",
+            "ghcr.io/opensafely-core/rstudio",
+        ]
+    )
+
+    assert run_main(rstudio, "--port 8787 --name test_rstudio") == 0
