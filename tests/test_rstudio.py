6707af48d18a8c3a3b003da7970231097fd9dcb3
bloodearnest <simon.davy@thedatalab.org>
2024-10-17 14:48:08 +0100

    Do not mount .gitconfig unless it exists.
    Or else, docker will create and empty version on the host, that will be
    owned by root in linux.

--- tests/test_rstudio.py
+++ tests/test_rstudio.py
@@ -2,37 +2,54 @@ import os
 import pathlib
 from sys import platform
 
+import pytest
+
 from opensafely import rstudio
 from tests.conftest import run_main
 
 
-def test_rstudio(run):
+@pytest.mark.parametrize("gitconfig_exists", [True, False])
+def test_rstudio(run, tmp_path, monkeypatch, gitconfig_exists):
+
+    home = tmp_path / "home"
+    home.mkdir()
+    # linux/macos
+    monkeypatch.setitem(os.environ, "HOME", str(home))
+    # windows
+    monkeypatch.setitem(os.environ, "USERPROFILE", str(home))
+
+    if gitconfig_exists:
+        (home / ".gitconfig").touch()
+
     if platform == "linux":
         uid = os.getuid()
     else:
         uid = None
 
-    run.expect(
-        [
-            "docker",
-            "run",
-            "--rm",
-            "--init",
-            "--label=opensafely",
-            "--platform=linux/amd64",
-            "--interactive",
-            "--user=0:0",
-            f"--volume={pathlib.Path.cwd()}://workspace",
-            "-p=8787:8787",
-            "--name=test_rstudio",
-            "--hostname=test_rstudio",
+    expected = [
+        "docker",
+        "run",
+        "--rm",
+        "--init",
+        "--label=opensafely",
+        "--platform=linux/amd64",
+        "--interactive",
+        "--user=0:0",
+        f"--volume={pathlib.Path.cwd()}://workspace",
+        "-p=8787:8787",
+        "--name=test_rstudio",
+        "--hostname=test_rstudio",
+        "--env=HOSTPLATFORM=" + platform,
+        f"--env=HOSTUID={uid}",
+    ]
+
+    if gitconfig_exists:
+        expected.append(
             "--volume="
             + os.path.join(os.path.expanduser("~"), ".gitconfig")
             + ":/home/rstudio/local-gitconfig",
-            "--env=HOSTPLATFORM=" + platform,
-            f"--env=HOSTUID={uid}",
-            "ghcr.io/opensafely-core/rstudio",
-        ]
-    )
+        )
+
+    run.expect(expected + ["ghcr.io/opensafely-core/rstudio"])
 
     assert run_main(rstudio, "--port 8787 --name test_rstudio") == 0
