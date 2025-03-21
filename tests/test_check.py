db080c346d56d3a11fdb44bdc1a0a7d2fe003e94
David Evans <dave.evans@thedatalab.org>
2025-03-21 10:28:32 +0000

    fix: Expand erhQL checks to cover function calls

--- tests/test_check.py
+++ tests/test_check.py
@@ -412,8 +412,10 @@ def test_check_cohort_datasets(
     [
         ("bad_name", "bad_name", False),
         ("bad_name.", "bad_name", True),
+        ("bad_name()", "bad_name", True),
         ("module.bad_name.", "bad_name", True),
-        ("not_a_bad_name.", "bad_name", False),
+        ("module.bad_name()", "bad_name", True),
+        ("not_a_bad_name()", "bad_name", False),
         ("# bad_name.", "bad_name", False),
     ],
 )
