07434d34c1e34912a311e93d9612c4e71320eab9
Becky Smith <becky.smith@thedatalab.org>
2026-08-04 10:44:01 +0100

    Pass strptime format directly to avoid ruff complaining
    Ruff incorrectly complains about "Naive datetime constructed using
    `datetime.datetime.strptime()` without %z" when the format is passed
    as a variable; we don't reuse it, so just pass it in directly.

--- tests/jobrunner/test_models.py
+++ tests/jobrunner/test_models.py
@@ -5,8 +5,7 @@ from tests.jobrunner.factories import job_factory
 
 def test_job_asdict_timestamps(db):
     actual_time = "2022-10-07T14:59:12.345678+0000"
-    fmt = "%Y-%m-%dT%H:%M:%S.%f%z"
-    dt = datetime.strptime(actual_time, fmt)
+    dt = datetime.strptime(actual_time, "%Y-%m-%dT%H:%M:%S.%f%z")
     ts = dt.timestamp()
 
     job = job_factory(created_at=int(ts), status_code_updated_at=int(ts * 1e9))
