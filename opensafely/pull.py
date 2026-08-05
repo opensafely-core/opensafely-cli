2bab0c29b47d9d69caf41473e80daa8815ef965e
Becky Smith <becky.smith@thedatalab.org>
2026-08-05 14:18:06 +0100

    Use removeprefix/removesuffix in place of lstrip/slice assignment (B005, FURB188)
    ruff rule B005: lstrip with multicharacter strings
    Using lstrip with multicharacter strings is misleading; what we
    actually mean by this is "remove the prefix". In reality, lstrip
    does what we want here, but it's potentially confusing.
    
    ruff rule FURB188: Use removeprefix instead of unnecessary conditional
    reassignment to slice

--- opensafely/pull.py
+++ opensafely/pull.py
@@ -215,7 +215,7 @@ def get_auth_token(header):
 
     Bearer realm="https://ghcr.io/token",service="ghcr.io",scope="repository:opensafely-core/busybox:pull"
     """
-    header = header.lstrip("Bearer")
+    header = header.removeprefix("Bearer")
     # split_header_words is weird, but better than doing it ourselves
     words = split_header_words([header])
     values = dict(next(zip(*words)))
